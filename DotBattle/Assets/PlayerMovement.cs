ea59d0f5ef2f84170d13f878797a09d7810d01c0
TomMGrello <tommgrello@gmail.com>
2018-02-25 13:58:56 -0500

    Transfer

--- DotBattle/Assets/PlayerMovement.cs
+++ DotBattle/Assets/PlayerMovement.cs
@@ -4,21 +4,35 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 	// Use this for initialization
-	float starting_z;
+	public float starting_z;
+	Vector3 touchPosition;
+	public GameObject marker;
+
 	void Start () {
-		starting_z = transform.position.z;
+		touchPosition = new Vector3 (0, 0, 0);
 	}
 
-	void FixedUpdate () {
-		Touch touch = Input.GetTouch (0);
+	void FixedUpdate() {
+		Touch[] touches = Input.touches;
+		for(int i = 0; i < touches.Length; i++){
+			// The screen has been touched so store the touch
+			Touch touch = touches[i];
 
-		if(touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved || touch.phase == TouchPhase.Began){
-			Vector3 touchedPos = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y, starting_z));
-			transform.position = Vector3.Lerp (transform.position, touchedPos, 5f);
+			if (touch.phase == TouchPhase.Stationary || touch.phase == TouchPhase.Moved) {
+				// If the finger is on the screen, move the object smoothly to the touch position
+				Vector3 touchPosition = Camera.main.ScreenToWorldPoint (new Vector3 (touch.position.x, touch.position.y, 10));   
+				if (i == 0) {
+					             
+					transform.position = Vector3.Lerp (transform.position, touchPosition, Time.deltaTime);
+				} else if (i == 1) {
+					Instantiate (marker, touchPosition, Quaternion.identity);
+				}
+			}
 		}
 	}
 
 	void OnGUI(){
-		GUI.Label (new Rect (100, 100, 200, 50), string.Format("X: {0}, Y: {1}, Z: {0}",transform.position.x,transform.position.y,transform.position.z));
+		GUI.Label (new Rect (100, 100, 300, 100), string.Format("X: {0}, Y: {1}, Z: {2}",transform.position.x,transform.position.y,transform.position.z));
+		GUI.Label (new Rect (100, 150, 300, 100), string.Format("X: {0}, Y: {1}, Z: {2}",touchPosition.x,touchPosition.y,touchPosition.z));
 	}
 }
